9700b33569106aebb2adef24c193e38ec5cbf309
Loïc Frémont <lc.fremont@gmail.com>
2020-09-18 11:42:23 +0200

    Init CI

---
+++ src/Component/Template/TemplateHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Mobizel package.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Mobizel\Component\MarkdownDocs\Template;
+
+final class TemplateHandler implements TemplateHandlerInterface
+{
+    /** @var string */
+    private $projectDir;
+
+    public function __construct(string $projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
+
+    public function getTemplatePath(string $slug): string
+    {
+        return $slug.'.md';
+    }
+
+    public function getTemplateAbsolutePath(string $slug): string
+    {
+        return $this->projectDir.'/'.$this->getTemplatePath($slug);
+    }
+}
