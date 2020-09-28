b3480549834fc9a0e7e8aa89de835d33220062a4
Loïc Frémont <lc.fremont@gmail.com>
2020-09-28 17:19:28 +0200

    Test page action

---
+++ tests/app/AppKernel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mobizel\Bundle\MarkdownDocsBundle\Tests\app;
+
+use Mobizel\Bundle\MarkdownDocsBundle\MobizelMarkdownDocsBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
+
+class AppKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        return [
+            new FrameworkBundle(),
+            new MobizelMarkdownDocsBundle(),
+            new TwigBundle(),
+            new TwigExtraBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config.yaml');
+    }
+}
