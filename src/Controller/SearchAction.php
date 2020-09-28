29f7af1cb230717eefb77316d0d05b7e835e6c73
Loïc Frémont <lc.fremont@gmail.com>
2020-09-28 11:38:59 +0200

    Add routes

---
+++ src/Controller/SearchAction.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of markdown-docs.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Mobizel\Bundle\MarkdownDocsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class SearchAction extends AbstractController
+{
+    /** @var string */
+    private $docsDir;
+
+    public function __construct(string $docsDir)
+    {
+        $this->docsDir = $docsDir;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $query = $request->get('query', '');
+
+        $finder = new Finder();
+        $finder->files()->in($this->docsDir)->contains($query);
+
+        return $this->render('@MobizelMarkdownDocs/search/index.html.twig', [
+            'files' => $finder,
+        ]);
+    }
+}
