a8042de4b225acd13d09460aa3e96b009542993a
Loïc Frémont <lc.fremont@gmail.com>
2020-12-07 20:54:59 +0100

    Fix coding standard

--- src/Markdown/MarkdownExtra.php
+++ src/Markdown/MarkdownExtra.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Mobizel package.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mobizel\Bundle\MarkdownDocsBundle\Markdown;
 
 use ParsedownExtra;
